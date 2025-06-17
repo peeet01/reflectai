4b107fa022bd70916fbb4b0efde6c253fe7bb581
peeet01 <peeet01@gmail.com>
2025-06-17 22:55:49 +0200

    Add files via upload

--- modules/predict_lorenz.py
+++ modules/predict_lorenz.py
@@ -1,5 +1,16 @@
 import numpy as np
+from sklearn.linear_model import Ridge
 
-def predict_lorenz(data, window=1):
-    # Egyszerű lineáris előrejelzés: x(t+1) = x(t)
-    return np.roll(data, -1, axis=0)
+def predict_lorenz(data, window=5, alpha=1.0):
+    X, y = [], []
+    for i in range(len(data) - window):
+        X.append(data[i:i+window].flatten())
+        y.append(data[i+window])
+
+    X, y = np.array(X), np.array(y)
+    model = Ridge(alpha=alpha)
+    model.fit(X, y)
+    predictions = model.predict(X)
+    # Visszatöltjük kezdeti értékekkel az eredeti hosszra
+    pad = np.zeros((window, y.shape[1]))
+    return np.vstack((pad, predictions))
