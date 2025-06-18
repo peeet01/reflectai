41bb7e08695724c840b968594a0f966347d4b193
peeet01 <peeet01@gmail.com>
2025-06-18 11:28:47 +0200

    Delete modules/graph_sync_analysis.py

--- modules/graph_sync_analysis.py
+++
@@ -1,7 +0,0 @@
-
-def run(mode='adaptive'):
-    import streamlit as st
-    if mode == 'adaptive':
-        st.header('Adaptív háló modul fut')
-    elif mode == 'scientific':
-        st.header('Tudományos kérdés modul fut')
