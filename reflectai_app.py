0df5d0ad81e1068cdd1bb6891d102968d4542807
peeet01 <peeet01@gmail.com>
2025-06-18 12:05:53 +0200

    Update reflectai_app.py

--- reflectai_app.py
+++ reflectai_app.py
@@ -25,28 +25,37 @@ menu = st.sidebar.selectbox("Válassz modult", (
     "Lorenz szimuláció",
     "Lorenz jóslás (MLP)",
     "Lorenz jóslás (egyszerű)",
-    "Gyakorlati graf elemzés",
-    "XOR predikció",
-    "Egyéb utils tesztek"
+    "Gyakorlati XOR predikció",
+    "Görbe szinkron analízis",
+    "Utils teszt",
 ))
 
 if menu == "Kuramoto szinkronizáció":
     kuramoto_sim.run()
+
 elif menu == "Kuramoto-Hebbian szimuláció":
     kuramoto_hebbiansim.run()
+
 elif menu == "Hebbian tanulás":
     hebbian_learning.run()
+
 elif menu == "Hebbian vizualizáció":
     hebbian_learning_viz.run()
+
 elif menu == "Lorenz szimuláció":
     lorenz_sim.run()
+
 elif menu == "Lorenz jóslás (MLP)":
     mlp_predict_lorenz.run()
+
 elif menu == "Lorenz jóslás (egyszerű)":
     predict_lorenz.run()
-elif menu == "Gyakorlati graf elemzés":
-    graph_sync_analysis.run()
-elif menu == "XOR predikció":
+
+elif menu == "Gyakorlati XOR predikció":
     xor_prediction.run()
-elif menu == "Egyéb utils tesztek":
+
+elif menu == "Görbe szinkron analízis":
+    graph_sync_analysis.run()
+
+elif menu == "Utils teszt":
     utils.run()
