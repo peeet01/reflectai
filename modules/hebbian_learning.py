21e837d8900a05f48cd36b372b97bdc213e740df
peeet01 <peeet01@gmail.com>
2025-06-18 14:36:30 +0200

    Update hebbian_learning.py

--- modules/hebbian_learning.py
+++ modules/hebbian_learning.py
@@ -1,2 +1,13 @@
+import streamlit as st
+import numpy as np
+
 def run():
-    st.write('Hebbian tanulás')
+    st.write("Hebbian tanulás modul fut.")
+
+    x = np.array([[1], [0], [1]])  # bemenet
+    y = np.array([[1], [0], [0]])  # kimenet
+    w = np.dot(y, x.T)            # Hebbian szabály: w = y * x^T
+
+    st.write("Bemenet (x):", x)
+    st.write("Kimenet (y):", y)
+    st.write("Súlymátrix (w):", w)
