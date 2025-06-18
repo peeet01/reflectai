29ea96f69a9633c7e7aafb15f38f822a1f506b5c
peeet01 <peeet01@gmail.com>
2025-06-19 00:21:18 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,3 +5,4 @@ from .graph_sync_analysis import run as run_graph
 from .kuramoto_hebbian_sim import run as run_kuramoto_hebbian
 from .lorenz_sim import run as run_lorenz
 from .predict_lorenz import run as run_lorenz_pred
+from .kuramoto_lstm import run as run_kuramoto_lstm
