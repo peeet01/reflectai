bc8f83782e58e64f6fd4a6e625bcbcc284c2a8d4
peeet01 <peeet01@gmail.com>
2025-06-18 21:23:16 +0200

    Update __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,7 @@
+from .kuramoto_sim import run as run_kuramoto
+from .hebbian_learning import run as run_hebbian
+from .xor_prediction import run as run_xor
+from .graph_sync_analysis import run as run_graph
+from .kuramoto_hebbian_sim import run as run_kuramoto_hebbian
+from .lorenz_sim import run as run_lorenz
+from .predict_lorenz import run as run_lorenz_pred
