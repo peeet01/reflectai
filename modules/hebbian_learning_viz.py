1655d28a32cb77069b891befca43408ac2fd355b
peeet01 <peeet01@gmail.com>
2025-06-18 14:47:30 +0200

    Update hebbian_learning_viz.py

--- modules/hebbian_learning_viz.py
+++ modules/hebbian_learning_viz.py
@@ -1,2 +1,23 @@
+import streamlit as st
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 def run():
-    st.write('Hebbian tanulás vizualizáció')
+    st.write("Hebbian tanulás vizualizáció modul fut.")
+
+    # Bemenet és kimenet mátrix
+    x = np.random.randint(0, 2, (3, 5))
+    y = np.random.randint(0, 2, (2, 5))
+
+    # Hebbian súlymátrix tanulás
+    w = y @ x.T
+
+    st.write("Bemeneti mátrix (x):")
+    st.write(x)
+    st.write("Kimeneti mátrix (y):")
+    st.write(y)
+
+    fig, ax = plt.subplots()
+    sns.heatmap(w, annot=True, cmap='coolwarm', ax=ax)
+    st.pyplot(fig)
