d75d2917ba45d076a00bfca669d6a9cef829476e
peeet01 <peeet01@gmail.com>
2025-06-18 13:47:38 +0200

    Update reflectai_app.py

--- reflectai_app.py
+++ reflectai_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from modules.kuramoto_sim import run as run_kuramoto
-from modules.kuramoto_hebbian import run as run_kuramoto_hebbian
+from modules.kuramoto_hebbiansim import run as run_kuramoto_hebbian
 from modules.hebbian_learning import run as run_hebbian
 from modules.graph_sync_analysis import run as run_graph_sync
 from modules.xor_prediction import run as run_xor
