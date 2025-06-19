6d2619ad5a6deb8253c50869758ac797f19449f6
peeet01 <peeet01@gmail.com>
2025-06-19 13:54:54 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,17 +1,14 @@
-# __init__.py – modulregisztráció
-
-__all__ = [
-    "kuramoto_sim",
-    "hebbian_learning",
-    "xor_prediction",
-    "kuramoto_hebbian_sim",
-    "topo_protect",
-    "lorenz_sim",
-    "predict_lorenz",
-    "berry_curvature",
-    "noise_robustness",
-    "esn_prediction",
-    "plasticity_dynamics",
-    "fractal_dimension",
-    "persistent_homology"  # 🆕 új modul
-]
+from modules.kuramoto_sim import run as run_kuramoto
+from modules.hebbian_learning import run as run_hebbian
+from modules.xor_prediction import run as run_xor
+from modules.kuramoto_hebbian_sim import run as run_kuramoto_hebbian
+from modules.topo_protect import run as run_topo_protect
+from modules.lorenz_sim import run as run_lorenz_sim
+from modules.predict_lorenz import run as run_lorenz_pred
+from modules.berry_curvature import run as run_berry
+from modules.noise_robustness import run as run_noise
+from modules.esn_prediction import run as run_esn
+from modules.plasticity_dynamics import run as run_plasticity
+from modules.fractal_dimension import run as run_fractal
+from modules.persistent_homology import run as run_homology
+from modules.lyapunov_spectrum import run as run_lyapunov
