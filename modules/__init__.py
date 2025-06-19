ca4aafb19fc1883da4fa94425859914dbb6a639e
peeet01 <peeet01@gmail.com>
2025-06-19 08:16:49 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -6,3 +6,4 @@ from .kuramoto_hebbian_sim import run as run_kuramoto_hebbian
 from .lorenz_sim import run as run_lorenz
 from .predict_lorenz import run as run_lorenz_pred
 from .esn_prediction import run as run_esn
+from .berry_curvature import run as run_berry
