cf83dd61178d5f95f9c1c4526da6264013bd835d
peeet01 <peeet01@gmail.com>
2025-06-19 08:51:38 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,5 +5,7 @@ from .graph_sync_analysis import run as run_graph
 from .kuramoto_hebbian_sim import run as run_kuramoto_hebbian
 from .lorenz_sim import run as run_lorenz
 from .predict_lorenz import run as run_lorenz_pred
+from .topo_protect import run as run_topo_protect
+from .berry_curvature import run as run_berry_curvature
 from .esn_prediction import run as run_esn
-from .berry_curvature import run as run_berry
+from .noise_robustness import run as run_noise_robustness  # 🆕 zajmodul
