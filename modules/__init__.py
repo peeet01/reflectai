12b0c348a230c8dc6c42ad215a62a584e09824be
peeet01 <peeet01@gmail.com>
2025-06-19 12:50:09 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,12 +1,17 @@
-from .kuramoto_sim import run as run_kuramoto
-from .hebbian_learning import run as run_hebbian
-from .xor_prediction import run as run_xor
-from .graph_sync_analysis import run as run_graph
-from .kuramoto_hebbian_sim import run as run_kuramoto_hebbian
-from .lorenz_sim import run as run_lorenz
-from .predict_lorenz import run as run_lorenz_pred
-from .topo_protect import run as run_topo_protect
-from .berry_curvature import run as run_berry_curvature
+# __init__.py
+from .berry_curvature import run as run_berry
 from .esn_prediction import run as run_esn
-from .noise_robustness import run as run_noise_robustness  # 🆕 zajmodul
-from .fractal_dimension import run as run_fractal_dimension
+from .fractal_dimension import run as run_fractal
+from .graph_sync_analysis import run as run_graph_sync
+from .hebbian_learning import run as run_hebbian
+from .hebbian_learning_viz import run as run_hebbian_viz
+from .kuramoto_hebbian_sim import run as run_kuramoto_hebbian
+from .kuramoto_sim import run as run_kuramoto
+from .lorenz_sim import run as run_lorenz_sim
+from .mlp_predict_lorenz import run as run_lorenz_pred
+from .noise_robustness import run as run_noise
+from .plasticity_dynamics import run as run_plasticity
+from .predict_lorenz import run as run_predict_lorenz
+from .topo_protect import run as run_topo_protect
+from .xor_prediction import run as run_xor
+from .persistent_homology import run as run_persistent_homology
