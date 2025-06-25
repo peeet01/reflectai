350f5519b2f3acae651e6c4267fae8bcf14d1248
peeet01 <peeet01@gmail.com>
2025-06-25 16:48:27 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,16 +1,1 @@
-from modules.kuramoto_sim import run as run_kuramoto
-from modules.hebbian_learning import run as run_hebbian
-from modules.xor_prediction import run as run_xor
-from modules.kuramoto_hebbian_sim import run as run_kuramoto_hebbian
-from modules.topo_protect import run as run_topo_protect
-from modules.lorenz_sim import run as run_lorenz_sim
-from modules.predict_lorenz import run as run_lorenz_pred
-from modules.berry_curvature import run as run_berry
-from modules.noise_robustness import run as run_noise
-from modules.esn_prediction import run as run_esn
-from modules.plasticity_dynamics import run as run_plasticity
-from modules.fractal_dimension import run as run_fractal
-from modules.persistent_homology import run as run_homology
-from modules.lyapunov_spectrum import run as run_lyapunov  # ✅ ABSZOLÚT import!
-from modules.memory_landscape import run as run_memory_landscape
-from modules.noise_robustness import run as run_noise
+
