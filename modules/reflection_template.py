1fed38b1de106ba2e6f8b88f486538dc1afd43e4
peeet01 <peeet01@gmail.com>
2025-06-25 03:10:47 +0200

    Update reflection_template.py

--- modules/reflection_template.py
+++ modules/reflection_template.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 def reflection_template_module():
     st.header("🔁 Reflexió sablon")
+    st.markdown("Töltsd ki az alábbi reflexiós sablont.")
     st.text_input("Mi történt ma?", key="reflection_today")
     st.text_input("Mit tanultál belőle?", key="reflection_learning")
