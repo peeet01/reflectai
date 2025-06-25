5f138152e76ffa38f8802ac82a9234fc7cb6dea6
peeet01 <peeet01@gmail.com>
2025-06-25 03:10:09 +0200

    Update journal.py

--- modules/journal.py
+++ modules/journal.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 def journal_module():
     st.header("📔 Kutatási napló")
+    st.markdown("Itt vezetheted a kutatási megfigyeléseidet.")
     st.text_area("Jegyzet", key="journal_notes")
