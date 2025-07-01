51408ff4e38b16854bdac1afb47f66108544b039
peeet01 <peeet01@gmail.com>
2025-07-01 10:11:13 +0200

    Update berry_curvature.py

--- modules/berry_curvature.py
+++ modules/berry_curvature.py
@@ -134,7 +134,7 @@ def run():
 
     col = st.columns([1])[0]
     with col:
-        st.markdown("### 🧭 d-vektor útvonala a Bloch-gömbön")
+    
         st.markdown("&nbsp;", unsafe_allow_html=True)
 
     st.plotly_chart(fig_d, use_container_width=True)
