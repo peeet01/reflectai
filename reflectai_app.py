234616bd78767fd309789a5755cbe1df28efd7c9
peeet01 <peeet01@gmail.com>
2025-07-02 22:08:55 +0200

    Update reflectai_app.py

--- reflectai_app.py
+++ reflectai_app.py
@@ -30,6 +30,7 @@ module_categories = {
         "XOR Prediction": "xor_prediction",
         "MLP Predict Lorenz": "mlp_predict_lorenz",
         "Oja Learning": "oja_learning",
+        "STDP Learning": "stdp_learning",
     },
     "⚗️ Szimulációk és dinamikák": {
         "Kuramoto Sim": "kuramoto_sim",
