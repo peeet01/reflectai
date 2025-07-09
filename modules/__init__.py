4fd8fdd7bb7660b8e460c4161a1af1cf74a1a43e
peeet01 <peeet01@gmail.com>
2025-07-09 21:13:16 +0200

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -64,5 +64,5 @@ registry = {
     "Fraktál Explorer": run_fractal_explorer,
     "Ising szimulácio": run_ising_sim,
     "Hebbian Learning Viz": run_hebbian_viz,
-    "Critical Hebbian (3D)": run_critical_hebbian,
+    "Critical Hebbian": run_critical_hebbian,
 }
