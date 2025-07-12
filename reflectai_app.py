08d705a63b1b601049765fe6da3d7be96dbe3273
peeet01 <peeet01@gmail.com>
2025-07-12 18:31:14 +0200

    Update reflectai_app.py

--- reflectai_app.py
+++ reflectai_app.py
@@ -76,7 +76,7 @@ with st.sidebar:
                 left: 50%;
                 transform: translateX(-50%);
                 width: 260px;
-                height: 160px;
+                height: 260px;
                 background: radial-gradient(circle, rgba(56,189,248,0.6) 0%, rgba(0,0,0,0) 80%);
                 filter: blur(28px);
                 z-index: 0;
