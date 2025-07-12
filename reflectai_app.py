7fd4b3139570f229f6e8361e8426f09c290693e5
peeet01 <peeet01@gmail.com>
2025-07-12 22:24:18 +0200

    Update reflectai_app.py

--- reflectai_app.py
+++ reflectai_app.py
@@ -72,20 +72,7 @@ selected_category = st.sidebar.radio("Kategória:", category_names)
 
 # 🏠 Kezdőlap
 if selected_category == main_menu:
-    st.markdown("""
-    <style>
-    .responsive-header {
-    width: 100%;
-    height: auto;
-    max-height: 700px;
-    object-fit: contain;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-}
-</style>
-<img src="static/nyitokep.png" class="responsive-header">
-""", unsafe_allow_html=True)
+    st.image("static/nyitokep.png", use_column_width=True)
     st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
     st.image("static/logo.png", width=180)
     st.markdown("<h3>Neurolab AI – Intelligens szimulációs platform</h3>", unsafe_allow_html=True)
