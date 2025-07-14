a86ab5809c576b6f88bfc164fd8f93d21c04d03c
peeet01 <peeet01@gmail.com>
2025-07-14 15:48:23 +0200

    Update reflectai_app.py

--- reflectai_app.py
+++ reflectai_app.py
@@ -50,7 +50,7 @@ module_categories = {
         "Noise Robustness": "noise_robustness",
         "Ising Sim": "ising_sim",
         "Boltzmann Machine": "boltzmann_machine",
-        "NeuroGAN Lab": "neurogan_lab",
+        "GAN Lab": "gan_lab",
         
     },
     "🧪 Adatfeltöltés és predikciók": {
