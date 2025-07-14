fbc42c07a56d7d92bc08f6fa05c8e0ff5f355dce
peeet01 <peeet01@gmail.com>
2025-07-14 15:04:43 +0200

    Update reflectai_app.py

--- reflectai_app.py
+++ reflectai_app.py
@@ -39,6 +39,7 @@ module_categories = {
         "ESN Prediction": "esn_prediction",
         "Critical Hebbian": "critical_hebbian",
         "Information Bottleneck": "information_bottleneck",
+        "NeuroGAN Lab": "neurogan_lab",
         
     },
     "⚗️ Szimulációk és dinamikák": {
@@ -50,7 +51,6 @@ module_categories = {
         "Noise Robustness": "noise_robustness",
         "Ising Sim": "ising_sim",
         "Boltzmann Machine": "boltzmann_machine",
-        "NeuroGAN Lab": "neurogan_lab",
         
     },
     "🧪 Adatfeltöltés és predikciók": {
